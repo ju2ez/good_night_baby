31707358e3ce4b7ff7dcd3e6ecfc3416dc1e8b2f
ju2ez <JulianHatzky@googlemail.com>
2019-03-03 13:40:52 +0100

    added unit-test (placeholder) for mainActivity message

--- app/src/main/java/com/julianhatzky/good_night_baby/MainActivity.kt
+++ app/src/main/java/com/julianhatzky/good_night_baby/MainActivity.kt
@@ -1,32 +1,12 @@
 package com.julianhatzky.good_night_baby
 
-import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/test/java/com/julianhatzky/good_night_baby/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.julianhatzky.good_night_baby
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+internal class MainActivityTest {
+
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onCreate() {
+
+    }
+
+    @Test
+    fun topMessage_isThere() {
+        assertEquals(5, 5)
+    }
+}
